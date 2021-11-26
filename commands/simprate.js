94974bd17b97052bdbf258521cceeece28f3bf3f
deshanbasu <deshanbasura3@gmail.com>
2021-11-26 22:27:58 +0530

    Debug complete simprate is working fine

--- commands/simprate.js
+++ commands/simprate.js
@@ -13,38 +13,17 @@ function simpRate() {
 module.exports.run = async (bot, message, args) => {
 
     try {
-        // if (message.mentions.members.first()) {
-
-        //     console.log(message.mentions.members.first().user.id);
-        // }
-
-        // else if (simpCache.get(message.author.id) != null) {
-            
-        //     rate = simpCache.get(message.author.id).e
-        //     await message.reply(`You are ${rate} simp in the simp Universe`)
-
-        // } else {
-
-            
-        //     rate = simpRate()
-        //     success = simpCache.set(message.author.id, { e: rate })
-
-
-        //     await message.reply(`You are ${rate} simp in the simp Universe`)
-        // }
 
         const userId = (message.mentions.members.first()) ? message.mentions.members.first().user.id : message.author.id
 
         let rate = simpCache.get(userId) ? simpCache.get(userId) : null 
 
-        console.log(rate);
 
         if (rate) {
             await message.reply(`You are ${rate.e} simp in the simp Universe`)
             
         } else {
             rate = simpRate()
-            console.log(userId);
             s = simpCache.set(userId, { e: rate })
 
             await message.reply(`You are ${rate} simp in the simp Universe`)
