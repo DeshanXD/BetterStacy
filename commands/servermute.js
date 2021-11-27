0b5d41e15653d5d021baeafcec4f61d218c1b458
deshanbasu <deshanbasura3@gmail.com>
2021-11-27 21:18:40 +0530

    Added a replying embed to !servermute

---
+++ commands/servermute.js
@@ -0,0 +1,46 @@
+const  {Discord, RichEmbed, MessageEmbed} = module.require('discord.js')
+const getUserFromMention = require("../util/userId");
+
+
+
+module.exports.run = async (bot, message, args) => {
+
+    let userId = getUserFromMention(message)
+    let userObject = bot.users.cache.get(userId)
+
+
+    const votemuteEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setAuthor(`${userObject.username}`, `${userObject.avatarURL()}`)
+        .setDescription(`${userObject} You are getting vote muted by people on ${message.member.voice} initiated by ${message.author} `);
+
+    
+    try {
+        
+        if (userId === message.author.id) {
+            await message.reply('Are you dumb? mention someone first!')
+        } else if (!message.member.voice.channelId) {
+
+            // console.log(message.member)
+            console.log(message.member.voice.channelId)
+            await message.reply('You need to be in voice channel in order to run this command!')
+        } else {
+            await message.channel.send({ embeds: [votemuteEmbed]});
+
+        }
+
+
+
+        
+    } catch (e) {
+        console.log(`!servermute commad debug: ${e}`);
+    }
+}
+
+
+// exporting the module
+
+module.exports.help= {
+    name: "servermute",
+    desc: " Server mute random people!"
+}
