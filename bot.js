311880a9e711181d48e3a5d06e5accec0207bb72
deshanbasu <deshanbasura3@gmail.com>
2021-11-29 13:20:42 +0530

    Fixed commands reactions handler

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const config = require('./config.json')
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+// I hate this intent shit
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_VOICE_STATES", "GUILD_MEMBERS"] });
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
--- commands/servermute.js
+++ commands/servermute.js
@@ -1,33 +1,61 @@
-const { Discord, RichEmbed, MessageEmbed } = module.require("discord.js");
+const {
+  Discord,
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+} = require("discord.js");
 const getUserFromMention = require("../util/userId");
 
 // exporting the module
 module.exports = {
   name: "servermute",
   description: " Server mute random people!",
+
   async execute(client, message, args) {
     let userId = getUserFromMention(message);
     let userObject = client.users.cache.get(userId);
-  
+
+    // console.log(userObject)
+
     const votemuteEmbed = new MessageEmbed()
       .setColor("#0099ff")
       .setAuthor(`${userObject.username}`, `${userObject.avatarURL()}`)
       .setDescription(
-        `${userObject} You are getting vote muted by people on ${message.member.voice} initiated by ${message.author} `
+        `${userObject} You are getting vote muted by people on ${message.member.voice.channel.name} initiated by ${message.author} `
       );
-  
+
     // TODO fix: identify the user's voice state to make the command only be available in the voice chat
     try {
       if (userId === message.author.id) {
         await message.reply("Are you dumb? mention someone first!");
-      } else if (!message.member.voice.channelId) {
-        // console.log(message.member)
-        console.log(message.member.voice.channelId);
-        await message.reply(
-          "You need to be in voice channel in order to run this command!"
-        );
+
+        } else if (!message.member.voice.channelId) {
+          await message.reply(
+            "You need to be in voice channel in order to run this command!"
+          );
       } else {
-        await message.channel.send({ embeds: [votemuteEmbed] });
+        let deliverd_message = await message.channel.send({
+          embeds: [votemuteEmbed],
+          ephemeral: true,
+          fetchReply: true,
+        });
+
+        await deliverd_message.react("✅");
+        
+        const filter = (reaction, user) => {
+
+          
+          console.log(message.member.voice.channel.members.forEach(m => m.guild.id))
+
+
+          return reaction.emoji.name === '✅' && user.id === message.member.voice.channel.members.forEach(m => m.user.id)
+
+        }
+        deliverd_message.awaitReactions({ filter, time: 10_000 })
+          .then(collected => console.log(`Collected ${collected.size} reactions`))
+          .catch(console.error);
+
+
       }
     } catch (e) {
       console.log(`!servermute commad debug: ${e}`);
